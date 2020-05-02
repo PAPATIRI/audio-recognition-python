ade9b782e62a72f33c4bf943682bd7a55104d34d
PAPATIRI <syariftaufik11@gmail.com>
2020-05-03 00:24:53 +0700

    play audio using pyaudio and wave python

--- audio.py
+++ audio.py
@@ -1,6 +1,27 @@
-import pyglet
+# import PyAudio
+import wave
 
-file = pyglet.resource.media('audio_notification.mp3')
-file.play()
 
-pyglet.app.run()
+def play(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = PyAudio.PyAudio()
+
+    stream = pa.open(
+        format=pa.get_format_from_width(wf.getsampwidth()),
+        channel=wf.getchannels(),
+        rate=wf.getframerate(),
+        output=True
+    )
+
+    data_stream = wf.readframes(chunk)
+
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+
+play('audio_notification.mp3')
